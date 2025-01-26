6cb855a15d851c7571e015b3ecadd4a9afa2cb58
Fran Rojo <franmorojo@gmail.com>
2025-01-26 18:38:01 -0500

    aw yeah

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import "./fran.css";
 import "./main.css";
 
 import * as THREE from "three";
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import GUI from 'lil-gui'; 
+
+const gui = new GUI();
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.getElementsByClassName("game")[0],
@@ -63,10 +67,61 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+const material = new THREE.MeshToonMaterial({ color: 0x00ffff});
+
+
+const box_geometry = new THREE.BoxGeometry(1, 1, 1);
+const cube = new THREE.Mesh(box_geometry, material)
+cube.translateX(-3)
+scene.add(cube)
+
+const sphere_geo = new THREE.SphereGeometry(1)
+const sphere = new THREE.Mesh(sphere_geo, material);
+sphere.translateX(3)
+scene.add(sphere);
+
+const loader = new GLTFLoader()
+
+// Load a glTF resource
+loader.load(
+	// resource URL
+	'assets/litwick.glb',
+	// called when the resource is loaded
+	function ( gltf ) {
+
+		scene.add( gltf.scene );
+
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
+
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( `Error loading model: ${error}` );
+
+	}
+);
+
+
+const light = new THREE.AmbientLight( 0xffffff, 0.1 ); // soft white light
+scene.add( light );
+
+const point_light = new THREE.PointLight( 0xffffff, 0.8);
+scene.add( point_light );
+
+gui.add(point_light.position, 'x')
 
 camera.position.z = 5;
 
