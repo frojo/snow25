340f3df4ffd9d8dd21d29b1fa8657f1bd6f929a5
Fran Rojo <franmorojo@gmail.com>
2025-02-02 18:33:56 -0500

    woo

---
+++ src/includes/ajeeb__coroutines.d.ts
@@ -0,0 +1,2 @@
+// have to do this to satisfy typescript apparently
+declare module '@ajeeb/coroutines';
--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,22 @@ import * as coro from '@ajeeb/coroutines';
 
 import * as THREE from "three";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import GUI from 'lil-gui'; 
+import { Sky } from 'three/addons/objects/Sky.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+import GUI from 'lil-gui';
+import { MathUtils, Vector3 } from "three";
+import { toneMapping } from "three/tsl";
 
 const gui = new GUI();
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.getElementsByClassName("game")[0],
 });
+renderer.setPixelRatio(window.devicePixelRatio)
 
-//renderer.setPixelRatio(window.devicePixelRatio)
+//gui.addColor(renderer, 'clearColor', 255)
+
 
 const ASPECT = 2 / 1;
 
@@ -23,7 +30,7 @@ interface Sizes {
   height: number;
 }
 
-let sizes : Sizes = {
+let sizes: Sizes = {
   width: 0,
   height: 0,
 }
@@ -54,6 +61,8 @@ window.addEventListener("resize", resize);
 
 
 // SET UP SCENE
+
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -62,85 +71,169 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', render);
+//controls.maxPolarAngle = Math.PI / rendee2;
+// controls.enableZoom = false;
+// controls.enablePan = false;
 
-const material = new THREE.MeshToonMaterial({ color: 0x00ffff});
+// set up sky
+const sky = new Sky();
+sky.scale.setScalar(450000);
 
+const phi = MathUtils.degToRad(-90);
+const theta = MathUtils.degToRad(-90);
+const sunPosition = new Vector3().setFromSphericalCoords(1, phi, theta);
+
+const uniforms = sky.material.uniforms;
+uniforms['turbidity'].value = 10;
+uniforms['rayleigh'].value = 3;
+uniforms['mieCoefficient'].value = 0.005;
+uniforms['mieDirectionalG'].value = 0.7;
+
+sky.material.uniforms.sunPosition.value = sunPosition;
+
+scene.add(sky);
+
+
+
+const textureLoader = new THREE.TextureLoader();
+const threeTone = textureLoader.load('assets/threeTone.jpg');
+threeTone.minFilter = THREE.NearestFilter;
+threeTone.magFilter = THREE.NearestFilter;
+
+const fiveTone = textureLoader.load('assets/fiveTone.jpg');
+fiveTone.minFilter = THREE.NearestFilter;
+fiveTone.magFilter = THREE.NearestFilter;
+
+
+const toonMaterial = new THREE.MeshToonMaterial({ color: 0x00ffff, gradientMap: fiveTone });
 
 const box_geometry = new THREE.BoxGeometry(1, 1, 1);
-const cube = new THREE.Mesh(box_geometry, material)
+const cube = new THREE.Mesh(box_geometry, toonMaterial)
 cube.translateX(-3)
 scene.add(cube)
 
 const sphere_geo = new THREE.SphereGeometry(1)
-const sphere = new THREE.Mesh(sphere_geo, material);
+const sphere = new THREE.Mesh(sphere_geo, toonMaterial);
 sphere.translateX(3)
 scene.add(sphere);
 
 const loader = new GLTFLoader()
 
+let litwick: THREE.Group | null = null;
 // Load a glTF resource
 loader.load(
-	// resource URL
-	'assets/litwick.glb',
-	// called when the resource is loaded
-	function ( model ) {
+  // resource URL
+  'assets/litwick.glb',
+  // called when the resource is loaded
+  function (model) {
 
-		scene.add( model.scene );
+    litwick = model.scene;
 
-		model.animations; // Array<THREE.AnimationClip>
-		model.scene; // THREE.Group
-		model.scenes; // Array<THREE.Group>
-		model.cameras; // Array<THREE.Camera>
-		model.asset; // Object
+    // toonify the material
+    litwick.traverse(function (o) {
+      if (o instanceof THREE.Mesh) {
+        const toonifiedMat = new THREE.MeshToonMaterial(
+          {
+            map: o.material.map,
+            gradientMap: fiveTone
+          });
 
-	},
-	// called while loading is progressing
-	function ( xhr ) {
+        o.material = toonifiedMat;
+      }
+    })
 
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 
-	},
-	// called when loading has errors
-	function ( error ) {
+    scene.add(litwick);
 
-		console.log( `Error loading model: ${error}` );
+    model.animations; // Array<THREE.AnimationClip>
+    model.scene; // THREE.Group
+    model.scenes; // Array<THREE.Group>
+    model.cameras; // Array<THREE.Camera>
+    model.asset; // Object
 
-	}
+  },
+  // called while loading is progressing
+  function (xhr) {
+
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+  },
+  // called when loading has errors
+  function (error) {
+
+    console.log(`Error loading model: ${error}`);
+
+  }
 );
 
 
-const light = new THREE.AmbientLight( 0xffffff, 0.1 ); // soft white light
-scene.add( light );
+// LIGHTS
 
-const point_light = new THREE.PointLight( 0xffffff, 0.8);
-point_light.position.setZ(1);
-scene.add( point_light );
+const ambient_light = new THREE.AmbientLight('white', 0.1); // soft white light
+scene.add(ambient_light);
 
-gui.add(point_light.position, 'z')
+// const point_light = new THREE.PointLight( 0xffffff, 2, 800);
+// point_light.position.setZ(1);
+// scene.add( point_light );
+
+// gui.add(point_light.position, 'z')
+
+const dirLight = new THREE.DirectionalLight('white', 2);
+const dLphi = MathUtils.degToRad(-70);
+const dLtheta = MathUtils.degToRad(-90);
+const dirLightPosition = new Vector3().setFromSphericalCoords(1, dLphi, dLtheta);
+
+dirLight.position.copy(dirLightPosition)
+dirLight.castShadow = true
+// dirLight.shadow.normalBias = 10
+// dirLight.shadow.camera.left = -500
+// dirLight.shadow.camera.right = 500
+// dirLight.shadow.camera.top = -500
+// dirLight.shadow.camera.bottom = 500
+scene.add(dirLight);
+
+const dirLightHelper = new THREE.DirectionalLightHelper(dirLight, 10);
+scene.add(dirLightHelper);
+
+
+
 
 camera.position.z = 2;
 
 resize();
 
+function render() {
+  renderer.render(scene, camera);
+}
 
 const SCHED = new coro.Schedule()
-function tick() {
-    SCHED.tick()
 
-  for (const obj of scene.children) {
-    obj.rotation.x += 0.01;
-    obj.rotation.y += 0.01;
+function* slowRotate(obj: THREE.Object3D | null) {
+  while (!obj)  {
+  // hmmmmmmmmmmmm
+  // what to do...
+    console.log("litwick")
+    yield
   }
-  renderer.render(scene, camera);
+
+  while (true) {
+    obj.rotation.x += 0.01
+    obj.rotation.y += 0.01
+    yield
+  }
+}
+
+SCHED.add(slowRotate(cube));
+SCHED.add(slowRotate(sphere));
+SCHED.add(slowRotate(litwick));
+
+function tick() {
+  SCHED.tick()
+
+  render();
 
   requestAnimationFrame(tick)
 }
 tick()
-
-// function animate() {
-
-
-//   renderer.render(scene, camera);
-// }
-// renderer.setAnimationLoop(animate);
